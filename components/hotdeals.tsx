edc0d3bcb45ae61864cfaa934ec338ae3468cc09
ubaidahkhan76 <ubaidahkhan.76@gmail.com>
2024-12-08 08:05:06 -0800

    Updated product descriptions

--- components/hotdeals.tsx
+++ components/hotdeals.tsx
@@ -29,14 +29,14 @@ export default function HotDeals() {
                         <FaRegHeart size={35} />
                     </div>
                 </div>
-                <div className="container1 bg-gray-100 p-3 rounded-xl border-8 border-transparent h-[500px] w-full ">
+                <div className="container1 bg-gray-100 p-3 rounded-xl border-8 border-transparent h-[500px] sm:h-[500px] md:h-[500px] lg:h-[500px] xl:h-[500px] w-full ">
                     <Image
                         src="/images/hotdeal2.jpg"
                         alt="deal1"
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">white Sneakers  Sports Shoes Lightweight Breathable Running Shoes</h1>
+                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">Stylish Sneakers Lightweight Durable Sports Shoes.</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:4000</h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:5000</h1>
@@ -54,7 +54,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">T-Shirts Pack Casual Cotton T-Shirts Available in 6 Colors</h1>
+                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">6-Color Casual Cotton T-Shirts Pack. Comfortable Fit</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:560 <span className="text-orange-600 text-xl">for each piece</span></h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:700</h1>
@@ -72,7 +72,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">Silver Necklace S12 Stylish Design with Adjustable Chain for Women</h1>
+                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">"Silver Necklace S12. Stylish Design Adjustable Chain.</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:960</h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:1200</h1>
@@ -90,7 +90,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">LuxGold Earrings Elegant Design with Secure Back Closure for Women</h1>
+                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">LuxGold Earrings Elegant Design Secure Back Closure.</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:19200</h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:24000</h1>
@@ -108,7 +108,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">Silver Ring Classic Design with Polished Finish for Men and Women</h1>
+                    <h1 className="text-2xl sm:text-2xl md:text-xl lg:text-2xl xl:text-2xl p-1">Silver Ring Classic Design Polished Finish for All.</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:440</h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:550</h1>
@@ -126,7 +126,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl p-1">School Bag b-20 with Multiple Compartments for Students</h1>
+                    <h1 className="text-2xl p-1">School Bag B-20 Multiple Compartments for Students</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:2400</h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:3000</h1>
@@ -144,7 +144,7 @@ export default function HotDeals() {
                         height={300}
                         width={400}
                         className="rounded-xl w-full h-[36vh]" />
-                    <h1 className="text-2xl p-1">Lipstick Long-Lasting Color with Smooth Finish Available in 3 Shades</h1>
+                    <h1 className="text-2xl p-1">Lipstick Long-Lasting Color Smooth Finish 3 Shades</h1>
                     <h1 className="text-blue-600 p-1 text-2xl">Rs:288 <span className="text-xl text-orange-600">for each piece</span></h1>
                     <div className="dicount flex gap-2">
                         <h1 className="p-1 text-gray-500 text-2xl line-through">Rs:360</h1>
